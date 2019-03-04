7d4762f2e5851dc0d585a262e94c237d0fdca653
Eren Gündüz <eren_clash@hotmail.com>
2019-03-04 18:30:50 +0300

    Add files via upload

---
+++ Lesson 2/listcheckbox/listcheckbox/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listcheckbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                textBox1.Text = checkBox1.Text;
+            if (checkBox2.Checked == false)
+                textBox1.Text = checkBox2.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                checkedListBox2.Items.Add(checkedListBox1.CheckedItems[i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 3;
+            textBox1.Text = comboBox1.Text;
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = checkedListBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.Text;
+        }
+    }
+}
